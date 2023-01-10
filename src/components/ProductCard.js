4f6350648d1c7e707df23dee0a4cccb8ac796b38
ShahariarRahman <shahariarrahman98@gmail.com>
2023-01-11 01:53:12 +0600

    Add to cart action complete !

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,9 +2,12 @@ import React from "react";
 import { BiListPlus } from "react-icons/bi";
 import { useLocation } from "react-router-dom";
 import { MdDeleteForever } from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../features/cart/cartSlice";
 
 const ProductCard = ({ product }) => {
   const { pathname } = useLocation();
+  const dispatch = useDispatch();
   return (
     <div className='shadow-lg relative rounded-3xl border p-3 flex flex-col text-indigo-900'>
       {pathname.includes("cart") && (
@@ -31,6 +34,7 @@ const ProductCard = ({ product }) => {
       <div className='flex gap-2 mt-5'>
         {!pathname.includes("cart") && (
           <button
+            onClick={() => dispatch(addToCart(product))}
             className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
           >
             Add to cart
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -7,7 +7,19 @@ const initialState = {
 const cartSlice = createSlice({
     name: "cart",
     initialState,
-    reducers: {},
+    reducers: {
+        addToCart: (state, action) => {
+            const selectedProduct = state.cart.find(product => product._id === action.payload._id);
+            if (!selectedProduct) {
+                const product = { ...action.payload, quantity: 1 };
+                state.cart.push(product);
+            }
+            else {
+                selectedProduct.quantity += 1;
+            };
+        },
+    },
 });
 
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
