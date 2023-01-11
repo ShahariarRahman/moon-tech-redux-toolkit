15581cb9f52df7e1ef378118a647f70660260679
ShahariarRahman <shahariarrahman98@gmail.com>
2023-01-11 08:35:25 +0600

    handle getProducts in UI using toolkit

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -2,24 +2,24 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../../components/ProductCard";
 import { toggle, toggleBrands } from "../../features/filter/filterSlice";
+import { getProducts } from "../../features/products/productsSlice";
 
 const Home = () => {
   const { stock, brands } = useSelector(state => state.filter);
+  const { products, isLoading } = useSelector(state => state.products);
 
   const dispatch = useDispatch();
 
-  const [products, setProducts] = useState([]);
-
-
   useEffect(() => {
-    fetch("http://localhost:5000/product")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
+    dispatch(getProducts())
   }, []);
 
   const activeClass = "text-white  bg-indigo-500 border-white";
 
   let content;
+  if (isLoading) {
+    content = <h1>Loading</h1>
+  };
 
   if (products.length) {
     content = products.map((product) => (
