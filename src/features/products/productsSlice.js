3158287e2af8cb1666750548bd3bba29c823c04c
ShahariarRahman <shahariarrahman98@gmail.com>
2023-01-11 06:36:17 +0600

    createAsyncthunk to getProducts,createAsyncThunk need two parameter, one is sliceName/ActionName, another one is an async function, for async need to use extraReducers instant of reducers

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -1,13 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     products: [],
 };
 
+
+const getProducts = createAsyncThunk("products/getProducts", async () => {
+
+});
+
 const productsSlice = createSlice({
     name: 'products',
     initialState,
-    reducers: {},
+    extraReducers: {},
 });
 
 export default productsSlice.reducer;
