0ba23370a896de3543233c1c5ddae27f0a1a5b69
ShahariarRahman <shahariarrahman98@gmail.com>
2023-01-11 08:14:32 +0600

    products/getProducts: addCase of actions pending, fulfilled, rejected

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -7,7 +7,7 @@ const initialState = {
     error: "",
 };
 
-const getProducts = createAsyncThunk("products/getProduct", async () => {
+export const getProducts = createAsyncThunk("products/getProducts", async () => {
     const res = await fetch("http://localhost:5000/product");
     const data = await res.json();
     return data;
@@ -17,10 +17,21 @@ const productsSlice = createSlice({
     name: 'products',
     initialState,
     extraReducers: (builder) => {
-        builder.addCase(getProducts.pending, (state, action) => {
-            state.isLoading = true;
-            state.isError = false;
-        });
+        builder
+            .addCase(getProducts.pending, (state, action) => {
+                state.isLoading = true;
+                state.isError = false;
+            })
+            .addCase(getProducts.fulfilled, (state, action) => {
+                state.products = action.payload;
+                state.isLoading = false;
+            })
+            .addCase(getProducts.rejected, (state, action) => {
+                state.products = [];
+                state.isLoading = false;
+                state.isError = true;
+                state.error = action.error.message;
+            });
     },
 });
 
