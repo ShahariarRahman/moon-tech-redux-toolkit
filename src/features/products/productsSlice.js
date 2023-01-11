b6b2f751d314677374b6198b25d055f030f01361
ShahariarRahman <shahariarrahman98@gmail.com>
2023-01-11 08:41:42 +0600

    clean previous async state management, to revise the asyne state management process using toolkit

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -2,37 +2,15 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     products: [],
-    isLoading: false,
-    isError: false,
-    error: "",
 };
 
-export const getProducts = createAsyncThunk("products/getProducts", async () => {
-    const res = await fetch("http://localhost:5000/product");
-    const data = await res.json();
-    return data;
-});
+
 
 const productsSlice = createSlice({
     name: 'products',
     initialState,
-    extraReducers: (builder) => {
-        builder
-            .addCase(getProducts.pending, (state, action) => {
-                state.isLoading = true;
-                state.isError = false;
-            })
-            .addCase(getProducts.fulfilled, (state, action) => {
-                state.products = action.payload;
-                state.isLoading = false;
-            })
-            .addCase(getProducts.rejected, (state, action) => {
-                state.products = [];
-                state.isLoading = false;
-                state.isError = true;
-                state.error = action.error.message;
-            });
-    },
+    reducers: {},
+
 });
 
 export default productsSlice.reducer;
